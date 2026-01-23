3a2371fd8d89fc6d058a7b1d9bf45a75ef1fb4a4
Maxime Luhat <133891284+Et3Aracno@users.noreply.github.com>
2026-01-23 02:13:34 +0100

    finished turret / Shoot
    Finished turret auto rotate to where you aim . Implemented the shoot mech fully , added VFX after actor destroy . Need to complete the targets and the map

--- MultiPlayer_Maxime_L/Source/MultiPlayer_Maxime_L/MultiPlayer_Maxime_L.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MultiPlayer_Maxime_L.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MultiPlayer_Maxime_L, "MultiPlayer_Maxime_L" );
-
-DEFINE_LOG_CATEGORY(LogMultiPlayer_Maxime_L)
--- MultiPlayer_Maxime_L/Source/MultiPlayer_Maxime_L/MultiPlayer_Maxime_LGameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MultiPlayer_Maxime_LGameMode.h"
-
-AMultiPlayer_Maxime_LGameMode::AMultiPlayer_Maxime_LGameMode()
-{
-	// stub
-}
